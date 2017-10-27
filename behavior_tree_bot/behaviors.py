db1ff5fffebc0eadc882b0ab68e4e1c66cff3406
nullP01nterException <jolina.lam@sbcglobal.net>
2017-10-26 18:44:02 -0700

    completed the assignment ははははは;should make the AI better though

--- behavior_tree_bot/behaviors.py
+++ behavior_tree_bot/behaviors.py
@@ -39,3 +39,70 @@ def spread_to_weakest_neutral_planet(state):
     else:
         # (4) Send half the ships from my strongest planet to the weakest enemy planet.
         return issue_order(state, strongest_planet.ID, weakest_planet.ID, strongest_planet.num_ships / 2)
+
+
+def aggressive_attack(state):
+    my_planets = iter(sorted(state.my_planets(), key=lambda p: p.num_ships))
+
+    enemy_planets = [planet for planet in state.enemy_planets()
+                      if not any(fleet.destination_planet == planet.ID for fleet in state.my_fleets())]
+    enemy_planets.sort(key=lambda p: p.num_ships)
+
+    target_planets = iter(enemy_planets)
+
+    try:
+        my_planet = next(my_planets)
+        target_planet = next(target_planets)
+        while True:
+            required_ships = target_planet.num_ships + \
+                                 state.distance(my_planet.ID, target_planet.ID) * target_planet.growth_rate + 1
+
+            if my_planet.num_ships > required_ships:
+                issue_order(state, my_planet.ID, target_planet.ID, required_ships)
+                my_planet = next(my_planets)
+                target_planet = next(target_planets)
+            else:
+                my_planet = next(my_planets)
+
+    except StopIteration:
+        return
+
+def defensive_defense(state):
+        my_planets = [planet for planet in state.my_planets()]
+        if not my_planets:
+            return
+
+        def strength(p):
+            return p.num_ships \
+                   + sum(fleet.num_ships for fleet in state.my_fleets() if fleet.destination_planet == p.ID) \
+                   - sum(fleet.num_ships for fleet in state.enemy_fleets() if fleet.destination_planet == p.ID)
+
+        avg = sum(strength(planet) for planet in my_planets) / len(my_planets)
+
+        weak_planets = [planet for planet in my_planets if strength(planet) < avg]
+        strong_planets = [planet for planet in my_planets if strength(planet) > avg]
+
+        if (not weak_planets) or (not strong_planets):
+            return
+
+        weak_planets = iter(sorted(weak_planets, key=strength))
+        strong_planets = iter(sorted(strong_planets, key=strength, reverse=True))
+
+        try:
+            weak_planet = next(weak_planets)
+            strong_planet = next(strong_planets)
+            while True:
+                need = int(avg - strength(weak_planet))
+                have = int(strength(strong_planet) - avg)
+
+                if have >= need > 0:
+                    issue_order(state, strong_planet.ID, weak_planet.ID, need)
+                    weak_planet = next(weak_planets)
+                elif have > 0:
+                    issue_order(state, strong_planet.ID, weak_planet.ID, have)
+                    strong_planet = next(strong_planets)
+                else:
+                    strong_planet = next(strong_planets)
+
+        except StopIteration:
+            return
